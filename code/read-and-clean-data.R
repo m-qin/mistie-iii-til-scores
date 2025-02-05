5fb2e6cdae4a35b14b2b3e6ee0a5132f15f912a6
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2025-02-05 13:39:20 -0500

    Update read-and-clean-data.R

--- code/read-and-clean-data.R
+++ code/read-and-clean-data.R
@@ -1,21 +1,50 @@
 
-# read in .dta file
+# Read in .dta file ----
 data <- haven::read_dta(here::here("data/private/M3_data_499_TIL.dta"))
 
-# clean data
+# Clean data ----
 data$studyname <- NULL # everything is "MISTIE III"
 data$modified_itt <- NULL # everything is 1
+# hmm maybe these are issues with reading in the data
 data$var30 <- NULL # everything is NA
 data$var31 <- NULL # everything is NA
 data$var32 <- NULL # everything is NA
-
-# "mis" column is the MISTIE treatment (250 treated, 249 control=standard of care)
+data$til_dnr_dni_order_baseline <- NULL # everything is NA
 
 # to do: more cleaning
 
-# maybe put in another R script: explore data
+# Explore data (maybe put in another R script) ----
+
+# Note: "mis" column is the MISTIE treatment (250 treated, 249 control=standard of care)
+
 # table(data$ischemic_stroke)
 # 0   1 
 # 426  73
 
-# to do: save cleaned dataset
+# note: I forget what the "til_", "D1score", and "D7score" prefixes mean and what INR means
+
+# table(data$til_dnr_dni_order_d7)
+# No Yes 
+# 20 457  22 
+
+# table(data$D1score_inr)
+# 0   1 
+# 490   9 
+# 
+# table(data$D7score_inr)
+# 0   1 
+# 488  11 
+# 
+# table(data$Baseline_INR)
+# 0   1   2 
+# 466  24   9 
+# 
+# table(data$D7_INR)
+# 0   1   2 
+# 462  13  11 
+# 
+# table(data$D7_DNR)
+# 0   1   2   3 
+# 489   4   2   4 
+
+# Save cleaned dataset (to do) ----
