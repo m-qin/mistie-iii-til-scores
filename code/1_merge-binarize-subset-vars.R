a7b0302afca0095d454d33ff66ddf6d7452fad1b
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2025-03-06 19:19:26 -0500

    binarize some variables with rare values

--- code/1_merge-binarize-subset-vars.R
+++ code/1_merge-binarize-subset-vars.R
@@ -7,6 +7,8 @@ if (!("data.table" %in% installed.packages())){
   install.packages("data.table")
 }
 
+library(data.table)
+
 ## Clear environment and Read in .dta files ----
 rm(list = ls())
 patients <- haven::read_dta(here::here("data/private/M3_data_499_TIL.dta"))
@@ -14,9 +16,21 @@ sites <- haven::read_dta(here::here("data/private/M3_TIL_added_data.dta"))
 
 ## Extract variables of interest and Merge ----
 source(here::here("code/helper-functions.R")) # includes groups of variables
+
 sites_subset <- subset(sites, select = c("new_id", cluster_vars))
-patients_subset <- subset(patients, select = c("patientnum_ninds", stratifying_vars, predictor_vars, outcome_var))
+patients_subset <- subset(patients, select = c("patientnum_ninds", stratifying_vars, orig_predictor_vars, outcome_var))
 data_merged <- merge(sites_subset, patients_subset, by.x = "new_id", by.y = "patientnum_ninds") # "new_id" is kept as a column
+
+## Binarize predictors with rare (i.e., <20) non-zero levels (models cannot predict levels they haven't seen) ----
+pred_vars_to_binarize <- c("Baseline_Hypotension",
+                           "Baseline_ICP",
+                           "Baseline_herniation",
+                           "D7_DNR")
+
+for (var in pred_vars_to_binarize){
+  data_merged[[paste0("Binarized_", var)]] <- 1 * (data_merged[[var]] > 0) # construct the binarized variable
+  data_merged[[var]] <- NULL # delete the old variable
+}
 
 ## Exclude observations with missing outcome ----
 data_analysis <- subset(data_merged, subset = !is.na(data_merged$glasgow_rankin_0_3_30)) # exclude 6 observations with missing outcome
--- code/helper-functions.R
+++ code/helper-functions.R
@@ -16,31 +16,56 @@ stratifying_vars <- c("age_at_consent", # integers (but treat as continuous), ra
                       "ich_deep_location") # binary, 192 values of 0, 307 values of 1
 
 # Note: These are all categorical; we will treat them as unordered factors in our models, even though they are generally ordered in increasing severity.
-predictor_vars <- c("BaselineNEWscore_BP", # ordinal categorical variable (values of 0, 1, 2, 3, 4, 5)
-                    "Day7NEWscore_BP",     # ordinal categorical variable (values of 0, 1, 2, 3, 4, 5); 13 NAs
-                    "Baseline_BP_control", # to do: treat as factor (4 levels)
-                    "D7_BP_control", # to do: treat as factor (4 levels); 13 NAs
-                    "Baseline_Hypotension", # 480 values of 0, 17 values of 1, 2 values of 2
-                    "D7_Hypotension", # 480 values of 0, 29 values of 1, 13 values of 2; 13 NAs
-                    "Baseline_Hyperpyrexia", # values of 0, 1, 2, 3; 13 NAs
-                    "Baseline_Hyperglycemia", # values of 0, 1, 2, 3
-                    "Baseline_ICP", # 487 values of 0, 8 values of 1, 1 value of 2, 2 values of 3; 1 NA
-                    "Baseline_herniation", # 484 values of 0, 8 values of 1, 6 values of 2; 1 NA
-                    "Baseline_INR", # 466 values of 0, 24 values of 1, 9 values of 2
-                    "D7_Hyperpyrexia", # values of 0, 1, 2, 3; 13 NAs
-                    "D7_Hyperglycemia", # values of 0, 1, 2, 3; 13 NAs
-                    "D7_ICP", # 454 values of 0, 15 values of 1, 9 values of 2, 7 values of 3, 1 value of 4; 13 NAs
-                    "D7_herniation", # 474 values of 0, 6 values of 1, 6 values of 2; 13 NAs
-                    "D7_INR", # 462 values of 0, 13 values of 1, 11 values of 2; 13 NAs
-                    "D7_DNR") # 489 values of 0, 4 values of 1, 2 values of 2, 4 values of 3
+orig_predictor_vars <- c("BaselineNEWscore_BP", # ordinal categorical variable (values of 0, 1, 2, 3, 4, 5)
+                         "Day7NEWscore_BP",     # ordinal categorical variable (values of 0, 1, 2, 3, 4, 5); 13 NAs
+                         "Baseline_BP_control", # to do: treat as factor (4 levels)
+                         "D7_BP_control", # to do: treat as factor (4 levels); 13 NAs
+                         "Baseline_Hypotension", # 480 values of 0, 17 values of 1, 2 values of 2
+                         "D7_Hypotension", # 444 values of 0, 29 values of 1, 13 values of 2; 13 NAs
+                         "Baseline_Hyperpyrexia", # values of 0, 1, 2, 3; 13 NAs
+                         "Baseline_Hyperglycemia", # values of 0, 1, 2, 3
+                         "Baseline_ICP", # 487 values of 0, 8 values of 1, 1 value of 2, 2 values of 3; 1 NA
+                         "Baseline_herniation", # 484 values of 0, 8 values of 1, 6 values of 2; 1 NA
+                         "Baseline_INR", # 466 values of 0, 24 values of 1, 9 values of 2
+                         "D7_Hyperpyrexia", # values of 0, 1, 2, 3; 13 NAs
+                         "D7_Hyperglycemia", # values of 0, 1, 2, 3; 13 NAs
+                         "D7_ICP", # 454 values of 0, 15 values of 1, 9 values of 2, 7 values of 3, 1 value of 4; 13 NAs
+                         "D7_herniation", # 474 values of 0, 6 values of 1, 6 values of 2; 13 NAs
+                         "D7_INR", # 462 values of 0, 13 values of 1, 11 values of 2; 13 NAs
+                         "D7_DNR") # 489 values of 0, 4 values of 1, 2 values of 2, 4 values of 3
 
+# Note: These are all categorical; we will treat them as unordered factors in our models, even though they are generally ordered in increasing severity.
+modif_predictor_vars <- c("BaselineNEWscore_BP", # ordinal categorical variable (values of 0, 1, 2, 3, 4, 5)
+                          "Day7NEWscore_BP",     # ordinal categorical variable (values of 0, 1, 2, 3, 4, 5); 13 NAs
+                          "Baseline_BP_control", # to do: treat as factor (4 levels)
+                          "D7_BP_control", # to do: treat as factor (4 levels); 13 NAs
+                          "Binarized_Baseline_Hypotension", # 480 values of 0, 19 values of 1
+                          "D7_Hypotension", # 444 values of 0, 29 values of 1, 13 values of 2; 13 NAs
+                          "Baseline_Hyperpyrexia", # values of 0, 1, 2, 3; 13 NAs
+                          "Baseline_Hyperglycemia", # values of 0, 1, 2, 3
+                          "Binarized_Baseline_ICP", # 487 values of 0, 11 values of 1; 1 NA
+                          "Binarized_Baseline_herniation", # 484 values of 0, 14 values of 1; 1 NA
+                          "Baseline_INR", # 466 values of 0, 24 values of 1, 9 values of 2
+                          "D7_Hyperpyrexia", # values of 0, 1, 2, 3; 13 NAs
+                          "D7_Hyperglycemia", # values of 0, 1, 2, 3; 13 NAs
+                          "D7_ICP", # 454 values of 0, 15 values of 1, 9 values of 2, 7 values of 3, 1 value of 4; 13 NAs
+                          "D7_herniation", # 474 values of 0, 6 values of 1, 6 values of 2; 13 NAs
+                          "D7_INR", # 462 values of 0, 13 values of 1, 11 values of 2; 13 NAs
+                          "Binarized_D7_DNR") # 489 values of 0, 10 values of 1
+
+# to do: rename this to cont_vars not quant_vars
 quant_vars <- c("age_at_consent", # integers (but treat as continuous), ranging from 28 to 90 (median 62, mean 61), unimodal, very slightly skew left
                 "gcs_randomization", # integers (but treat as continuous), ranging from 3 to 15 (median 10, mean 10.6), bimodal (around 8 and 13); high score is good
                 "nihss_randomization", # integers (but treat as continuous), ranging from 1 to 40 (median 19, mean 19), roughly normal
                 "stabct_ich_volume", # continuous, ranging from 20.9 to 127.1
-                "stabct_ivh_volume")
+                "stabct_ivh_volume") # continuous, ranging from 0 to 61.8
 
-cat_vars <- predictor_vars
+bin_vars <- c("Binarized_Baseline_Hypotension",
+              "Binarized_Baseline_ICP",
+              "Binarized_Baseline_herniation",
+              "Binarized_D7_DNR")
+
+cat_vars <- modif_predictor_vars[!(modif_predictor_vars %in% bin_vars)]
 
 
 ## Functions ----
